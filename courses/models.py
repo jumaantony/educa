77f5a16fa5c3292f08fa71c055dde5db959c789e
Jumaantony <jumaantony840gmail.com>
2022-08-09 14:10:56 +0300

    made a few tweaks

--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 
 # Create your models here.
@@ -43,6 +44,7 @@ class Module(models.Model):
 
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
 
     def __str__(self):
         return self.title
@@ -54,24 +56,25 @@ class Content(models.Model):
                                on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType,
                                      on_delete=models.CASCADE,
-                                     limit_choices_to={'model__in':(
-                                                    'text',
-                                                    'video',
-                                                    'image',
-                                                    'file')})
+                                     limit_choices_to={'model__in': (
+                                         'text',
+                                         'video',
+                                         'image',
+                                         'file')})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
 
 
 class ItemBase(models.Model):
     owner = models.ForeignKey(User, related_name='%(class)s_related',
-                                on_delete=models.CASCADE)
+                              on_delete=models.CASCADE)
     title = models.CharField(max_length=20)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
+
     def __str__(self):
         return self.title
 
