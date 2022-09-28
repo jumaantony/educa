19c19544f685123b19b062e2a5bfde2f6be15652
jumaantony <jumaantony840@gmail.com>
2022-09-28 07:21:00 +0300

    serving media files

--- educa/settings.py
+++ educa/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# serving media files
+MEDIA_ROOT = 'media'
+MEDIA_URL = 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
