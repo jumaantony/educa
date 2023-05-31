8179342596efe8d412da4c681dc6feeea52f3e10
Juma Antony <jumaantony840@gmail.com>
2023-05-31 05:50:22 +0300

    creating views and mixins

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
+
+
+from .models import Course
 
 # Create your views here.
+class OwnerMixin:
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+    
+class OwnerEditMixin:
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+    
+class OwnerCourseMixin(OwnerMixin):
+    model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+    
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    template_name = 'courses/manage/course/form.html'
+    
+   
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    template_name = 'courses/manage/course/list.html'
+    
+    # # Override the get_queryset() method to return only courses created by the current user.
+    # # this prevents users from editing courses they didn't create.
+    # def get_queryset(self):
+    #     qs = super().get_queryset()
+    #     return qs.filter(owner=self.request.user)  
+    
+    
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass
+
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    pass
+
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = 'courses/manage/course/delete.html'
+    
+ 
